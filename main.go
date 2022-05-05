75dfa277dcd7956d32083f7fd68a1807a4583eec
Naveen Kunareddy <gosmartwizard@gmail.com>
2022-05-05 10:30:56 +0530

    Reorganized the import statements

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1> Welcome to the awesome and aha site!<h1>")
 }
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
