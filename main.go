27bd0c9ff1949bd90647bd33f0881cf832fe2d35
Naveen Kunareddy <gosmartwizard@gmail.com>
2022-05-05 10:25:26 +0530

    Added the first web application server

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "net/http"
 
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1> Welcome to the awesome and aha site!<h1>")
+}
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
