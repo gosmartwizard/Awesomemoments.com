17317b914043e370ec1c0edd0aa6d1f21fc65423
Naveen Kunareddy <gosmartwizard@gmail.com>
2022-07-11 09:28:37 +0530

    Added the Header content type

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintln(w, "<h1> Welcome to the awesome and aha site!<h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("Starting the server on port:4949")
+	http.ListenAndServe(":4949", nil)
 }
