4cd4bd7e737705c5bf07e35e6105d85d9ffeec93
Naveen Kunareddy <gosmartwizard@gmail.com>
2022-07-12 10:28:07 +0530

    Added code for cross side scripting

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"os"
 )
 
 type User struct {
-	Name string
-	Age  int
+	//Bio string
+	Bio template.HTML
 }
 
 func main() {
@@ -18,26 +17,11 @@ func main() {
 	}
 
 	user := User{
-		Name: "Karthikeya",
-		Age:  6,
+		Bio: `<script>alert("Haha, you have been h4x0r3d!");</script>`,
 	}
 
 	err = t.Execute(os.Stdout, user)
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println()
-
-	user = User{
-		Name: "Ishaan",
-		Age:  3,
-	}
-
-	err = t.Execute(os.Stdout, user)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println()
 }
