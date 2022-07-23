d505d7d05811b7c454c7a9a34cdbd814377e9934
Naveen Kunareddy <gosmartwizard@gmail.com>
2022-07-23 14:23:16 +0530

    Added the sign in view and a controller to render it

--- main.go
+++ main.go
@@ -44,8 +44,11 @@ func main() {
 	r.Get("/helpline", controllers.StaticHandler(tpl))
 
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
+
 	r.Get("/signup", usersC.New)
 	r.Post("/signup", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	r.NotFound(func(writer http.ResponseWriter, request *http.Request) {
 		http.Error(writer, "Page not found", http.StatusNotFound)
